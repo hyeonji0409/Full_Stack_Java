48ef8fa08da9ab91387d8200ba4c701516c7ef89
hyeonji0409 <guswl8507@naver.com>
2021-11-16 17:56:55 +0900

    java day 12

--- Java_Day11/src/Baekjoon/Baekjoon2675.java
+++ Java_Day11/src/Baekjoon/Baekjoon2675.java
@@ -6,12 +6,11 @@ public class Baekjoon2675 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // 문자 알파벳 처리
         int r = scan.nextInt();
         String word = scan.next();
         String[] array_word;
         array_word = word.split("");
-
-
 
         for(int i = 0; i<array_word.length;i++){
             for(int j = 0; j <r; j++){
