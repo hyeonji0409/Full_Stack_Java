8f608e12d224dd7c9d1afb6e9da9016cbb064b9b
hyeonji0409 <guswl8507@naver.com>
2021-11-17 19:22:20 +0900

    java day 13

---
+++ javaProject1/src/ProductTest.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class Product {
+    Scanner scan = new Scanner(System.in);
+    int prdNo;
+    String prdName;
+    int prdPride;
+    int prdYear;
+    String prdMaker;
+
+    public Product(int no, String name, int pride, int year, String maker) {
+        this.prdNo = prdNo;
+        this.prdName = prdName;
+        this.prdPride = prdPride;
+        this.prdYear = prdYear;
+        this.prdMaker = prdMaker;
+    }
+
+    public String toString() {
+
+    }
+}
+
+class ProductTest {
+
+}
+
+public class javaProject1 {
+
+    public static void main(String[] args) {
+
+    }
+}
